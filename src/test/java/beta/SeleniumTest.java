196d20e6ad699be721f91801dc6df8911cb1fc15
sokrat <sokrat660@gmail.com>
2020-12-15 16:16:02 -0500

    first commit

---
+++ src/test/java/beta/SeleniumTest.java
@@ -0,0 +1,12 @@
+package beta;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+
+    @Test
+    public void getTest(){
+        System.out.println("Hello this is test");
+    }
+}
